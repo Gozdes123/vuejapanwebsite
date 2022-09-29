55e620deb621457f6fffda92ebf047d974c00dee
WilsonCheng <reborn2616am@gmail.com>
2022-09-29 16:16:58 +0800

    0929img hover

--- src/components/SecondPart.vue
+++ src/components/SecondPart.vue
@@ -2,21 +2,40 @@
   <div class="bg-[#f8f8f5] inline-block">
     <div class="flex gap-6 w-[80%] m-auto">
       <div class="w-[33%] mt-10">
-        <img src="../assets/japanwebsite/osuimono-monaka_bn.jpg" alt="" />
+        <div class="overflow-hidden">
+          <img
+            src="../assets/japanwebsite/osuimono-monaka_bn.jpg"
+            alt=""
+            class="scale-100 ease-out duration-500 hover:scale-105"
+          />
+        </div>
+
         <p class="text-[20px] mt-5">至福の一椀「お吸物もなか」</p>
         <p class="text-[14px] text-[#76716f]">
           「もなかの皮」にまでこだわった、風味豊かなお吸物です
         </p>
       </div>
       <div class="w-[33%] mt-10">
-        <img src="../assets/japanwebsite/pickup_img_02.jpg" alt="" />
+        <div class="overflow-hidden">
+          <img
+            src="../assets/japanwebsite/pickup_img_02.jpg"
+            alt=""
+            class="scale-100 ease-out duration-500 hover:scale-105"
+          />
+        </div>
         <p class="text-[20px] mt-5">だしとスパイスの魔法</p>
         <p class="text-[14px] text-[#76716f]">
           洋食も、だしでグングン美味しくなる
         </p>
       </div>
       <div class="w-[33%] mt-10">
-        <img src="../assets/japanwebsite/kaoruajidashi_bn.jpg" alt="" />
+        <div class="overflow-hidden">
+          <img
+            src="../assets/japanwebsite/kaoruajidashi_bn.jpg"
+            alt=""
+            class="scale-100 ease-out duration-500 hover:scale-105"
+          />
+        </div>
         <p class="text-[20px] mt-5">本枯鰹節 薫る味だし</p>
         <p class="text-[14px] text-[#76716f]">
           ひきたての「だし」の美味しさがいつでもご家庭で味わえる、香りと味にこだわり抜いた本格だしパック
